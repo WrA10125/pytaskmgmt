2d97dc33f636086affc5860ec3510fa0fbea3f21
WrA10125 <ajaygore10326@gmail.com>
2025-01-21 21:41:55 +0530

    first commit

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from database import mongo
 from routes.task_routes import task_bp
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:4200"], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+CORS(app, origins=["https://angtaskmgmt.vercel.app/"], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 @app.route('/api/tasks', methods=['OPTIONS'])
 def handle_options():
     response = jsonify({'message': 'CORS Pre-flight request'})
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:4200')
+    
+    response.headers.add('Access-Control-Allow-Origin', 'https://angtaskmgmt.vercel.app/')
     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     return response
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/techstack"
+app.config["MONGO_URI"] = "mongodb+srv://ajaygore:Ajaygore%404046@aava.gb3aw.mongodb.net/techstack?retryWrites=true&w=majority"
 mongo.init_app(app)
 
 app.register_blueprint(task_bp, url_prefix="/api/tasks")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True)
